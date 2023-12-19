a6c556ba96ff605f505f17fb93b3b67aa255d09a
Adrien Reuzeau <adrien.reuzeau@gmail.com>
2023-12-19 18:39:32 +0100

    increase canvas size, change bg colors

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { drawServerTiles, incConnectionCount, writeServerTile } from "./firebase.js";
 
 // BEGIN SETTINGS
-const canvasHeight = 400;
+const canvasHeight = 480;
 const canvasWidth = Math.floor(canvasHeight * (29.7 / 21.0)); // A4 ratio
 const defaultCanvasColor = "#FFFFFF";
 
