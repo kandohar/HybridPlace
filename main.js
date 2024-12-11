185cc42ca78705190677182101c47e4a9ffb0ed9
Adrien Reuzeau <adrien.reuzeau@gmail.com>
2024-12-11 19:14:49 +0100

    increase snapshot delay to 5s

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ document.getElementById("loader").style.display = 'none';
 document.getElementById("loaded").style.display = 'block';
 
 // wait for data to load and try upload snapshot
-setTimeout(tryUploadSnapshot, 2000);
+setTimeout(tryUploadSnapshot, 5000);
 
 // INITS
 function initUser() {
