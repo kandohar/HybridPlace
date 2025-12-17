768751e1c7831878b28b2cb614ee2f06aeba9525
Adrien Reuzeau <adrien.reuzeau@gmail.com>
2025-12-17 18:55:24 +0100

    increase canvas size to 260

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import { drawServerTiles, writeServerTile, writeServerTiles, incConnectionCount,
 import { uploadImage } from './supabase.js';
 
 // BEGIN SETTINGS
-const canvasHeight = 200;
+const canvasHeight = 260;
 const canvasWidth = Math.floor(canvasHeight * (29.7 / 21.0)); // A4 ratio
 const defaultCanvasColor = "#ffffff";
 
-const initialZoom = 4;
+const initialZoom = 3;
 
 const exportPixelSize = 8;
 
